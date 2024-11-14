ed0fcd8ab3804b9953ccb452726b55df5aa77544
Yuto Nishida <yuto@berkeley.edu>
2024-11-14 15:46:33 -0800

    Format

--- flake.nix
+++ flake.nix
@@ -5,7 +5,9 @@
     devenv.url = "github:cachix/devenv";
     devenv.inputs.nixpkgs.follows = "nixpkgs";
     fenix.url = "github:nix-community/fenix";
-    fenix.inputs = { nixpkgs.follows = "nixpkgs"; };
+    fenix.inputs = {
+      nixpkgs.follows = "nixpkgs";
+    };
   };
 
   nixConfig = {
@@ -13,62 +15,68 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
     let
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      packages = forEachSystem (system: 
-        let 
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+      packages = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
           metadata = builtins.fromTOML (builtins.readFile ./app/Cargo.toml);
         in
         {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-        devenv-test = self.devShells.${system}.default.config.test;
-        dark-notify = pkgs.rustPackages.rustPlatform.buildRustPackage {
-          pname = metadata.package.name;
-          version = metadata.package.version;
-          src = ./app;
-          buildInputs = [
-            pkgs.darwin.apple_sdk.frameworks.AppKit
-          ];
-          cargoLock = {
-            lockFile = ./app/Cargo.lock;
-          };
-        };
-        default = self.packages.${system}.dark-notify;
-      });
-
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = [
-                    pkgs.hello
-                    pkgs.darwin.apple_sdk.frameworks.AppKit
-                  ];
-
-                  enterShell = ''
-                    hello
-                  '';
-
-                  languages.rust = {
-                    enable = true;
-                  };
-                }
-              ];
+          devenv-up = self.devShells.${system}.default.config.procfileScript;
+          devenv-test = self.devShells.${system}.default.config.test;
+          dark-notify = pkgs.rustPackages.rustPlatform.buildRustPackage {
+            pname = metadata.package.name;
+            version = metadata.package.version;
+            src = ./app;
+            buildInputs = [ pkgs.darwin.apple_sdk.frameworks.AppKit ];
+            cargoLock = {
+              lockFile = ./app/Cargo.lock;
             };
-          });
+          };
+          default = self.packages.${system}.dark-notify;
+        }
+      );
+
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                # https://devenv.sh/reference/options/
+                packages = [
+                  pkgs.hello
+                  pkgs.darwin.apple_sdk.frameworks.AppKit
+                ];
+
+                enterShell = ''
+                  hello
+                '';
+
+                languages.rust = {
+                  enable = true;
+                };
+              }
+            ];
+          };
+        }
+      );
       formatter = forEachSystem (
         system:
         let
